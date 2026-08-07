0159ceb4a253c73a906bf17eccb647c4569dc4a1
Tushar Sitapara <tushar.sitapara152295@marwadiuniversity.ac.in>
2026-08-07 10:43:38 +0530

    All Work Done

---
+++ UNITS/ex3.py
@@ -0,0 +1,10 @@
+#. 3. Write a program to perform arithmetic relational and logical operations using Python operators.
+
+x = int(input("Enter Number 1 Here : "))
+y = int(input("Enter Number 2 Here : "))
+
+print("Addition is : ",x+y)
+print("Substrection is : ",x-y)
+print("Multipliction is : ",x*y)
+print("Division is : ",x/y)
+print("Modulo is : ",x%y)
