b45a472109378a7a7a95830ec7862e0dd030920b
Tushar Sitapara <tushar.sitapara152295@marwadiuniversity.ac.in>
2026-08-10 10:13:46 +0530

    Add files via upload

---
+++ UNITS/ex4.py
@@ -0,0 +1,35 @@
+# 4. Write a program to demonstrate string operations including slicing formatting and built-in string functions.
+
+x = input("Enter String Here : ")
+
+print(x[1:3])
+
+print(x.upper())
+
+print(x.lower())
+
+print(x.capitalize())
+
+print(x.title())
+
+print(x.strip())
+
+print(x.replace(x,"Sitapara"))
+
+print("Python".find("o"))
+
+print("This is Python".count("i"))
+
+print(x.split(" "))
+
+print("-".join(x))
+
+print("Python".startswith("Py"))
+
+print("Python".endswith("on"))
+
+print(x.isalpha())
+
+print(x.isdigit())
+
+print(len(x))
